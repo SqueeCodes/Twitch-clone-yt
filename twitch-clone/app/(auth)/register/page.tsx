5b5effb67de60cb7e3482879e072f99ea60de1e6
David <itsmesquee@gmail.com>
2024-08-15 09:21:21 -0600

    added pages and layouts using tsx

---
+++ twitch-clone/app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+
+const Page = () => {
+  return (
+    <div>
+      RegisterPage
+    </div>
+  )
+};
+
+export default Page;
--- twitch-clone/app/folder/route.ts
+++
@@ -1,6 +0,0 @@
-
-export function GET() {
-  return Response.json({ foo: "bar" });
-};
-
-
